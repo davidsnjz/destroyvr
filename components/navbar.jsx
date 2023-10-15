26a0eda21bd53b84a05884a27755bbdf9b650234
davidsnjz <davidsnjaz@gmail.com>
2023-10-15 13:41:43 -0600

    navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,67 +1,64 @@
-import { Link, useNavigate } from 'react-router-dom'
-import ultimateDestroyIMG from '../assets/Images/ISO.svg'
 import { useEffect, useState } from 'react';
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebase/firebase';
+import ultimateDestroyIMG from '../assets/Images/ISO.svg'
+import Cookies from 'js-cookie';
 
 function Navbar() {
+  const [session, setSession] = useState(false);
 
-    const [session, setSession] = useState(false);
+  const token = Cookies.get('token');
 
-    const token = localStorage.getItem('token');
+  const handleLogout = async () => {
+    await signOut(auth);
+    Cookies.remove('token');
+    Cookies.remove('user');
+    sessionHandle();
+    window.location.href = '/'; // Redirigir a la página de inicio
+  };
 
-    const history = useNavigate();
-
-    const handleLogout = async () => {
-        await signOut(auth);
-        localStorage.removeItem('token');
-        localStorage.removeItem('user');
-        sessionHandle();
-        history('/')
+  const sessionHandle = () => {
+    if (token) {
+      setSession(true);
+      console.log('token exists');
+    } else {
+      setSession(false);
+      console.log('token does not exist');
     }
+  };
 
-    const sessionHandle = () => {
-        if (token) {
-            setSession(true);
-            console.log('token exists')
-        }else{
-            setSession(false);
-            console.log('token does not exists')
-        }
-    }
+  useEffect(() => {
+    sessionHandle();
+  }, []);
 
-    useEffect(() => {
-        sessionHandle();
-    })
-
-    return (
-        <Router>
-        <div className="flex items-center w-full bg-slate-700 py-2 px-8 fixed">
-            <div className='flex w-full'>
-                <div className='flex justify-start'>
-                    <img src={ultimateDestroyIMG} className='h-14 w-auto' />
-                </div>
-               
-                <div className="w-full flex items-center justify-end">
-                    {session ?
-                        <div className='flex gap-4'>
-                            <Link to={"/Home"} className='buttons'>
-                                Home
-                            </Link>
-                            <button onClick={handleLogout} className="text-white font-bold border py-2 px-4 rounded-lg hover:bg-slate-800 transition duration-300">
-                                Logout
-                            </button>
-                        </div>
-                        :
-                        <Link to={"/login"} className='buttons'>
-                            Sign In
-                        </Link>
-                    }
-                </div>
-            </div>
+  return (
+    <div className="flex items-center w-full bg-slate-700 py-2 px-8 fixed">
+      <div className="flex w-full">
+        <div className="flex justify-start">
+          
         </div>
-        </Router>
-    )
+        <div className="w-full flex items-center justify-end">
+          {session ? (
+            <div className="flex gap-4">
+              <a href="/Home" className="buttons">
+                Home
+              </a>
+              <button
+                onClick={handleLogout}
+                className="text-white font-bold border py-2 px-4 rounded-lg hover:bg-slate-800 transition duration-300"
+              >
+                Logout
+              </button>
+            </div>
+          ) : (
+            <a href="/login" className="buttons">
+              Sign In
+            </a>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
