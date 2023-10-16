f6f4a2d90f695c79714921621ce7fc924c08f836
NullBylatolk <tlatolk.aguilar@gmail.com>
2023-10-15 18:30:16 -0600

    css

--- pages/Register.jsx
+++ pages/Register.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 //import Navbar from "../components/navbar";
+
 import { useRouter } from "next/router";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth, saveUserWallet } from "./api/firebase/firebase";
